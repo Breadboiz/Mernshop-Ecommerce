1db530778877e434ef04f1ae066a6d62bd6b9884
Khanhhuynh <khanhhuynh3508@gmail.com>
2025-04-22 03:37:59 +0700

    fix minor bug and add analystics chart

--- frontend/src/components/CreateProductForm/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm/CreateProductForm.jsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 const CreateProductForm = ({ onSubmit }) => {
   const [loading, setloading] = useState(false);
-  const [errors, setErrors] = useState({});
+
   const [formData, setFormData] = useState({
     product_name: '',
     product_brand: '',
@@ -43,9 +43,9 @@ const CreateProductForm = ({ onSubmit }) => {
     setloading(true);
 
     const form = new FormData();
-    form.append("product_name", formData.product_name);
-    form.append("product_brand", formData.product_brand);
-    form.append("product_price", Number(formData.product_price));
+    form.append("product_name", formData.product_name.trim());
+    form.append("product_brand", formData.product_brand.trim());
+    form.append("product_price", Number(formData.product_price.trim()));
     form.append("product_inStock", Number(formData.product_inStock));
     form.append("product_sold", Number(formData.product_sold));
     form.append("product_category", formData.product_category);
@@ -180,3 +180,202 @@ const Select = ({ label, name, value, onChange, options, error }) => (
 
 export default CreateProductForm;
 
+
+
+
+// const CreateProductForm = () => {
+//   const [loading, setloading] = useState(false);
+//   const [product, setProduct] = useState({
+//     product_name: "",
+//     product_brand: "",
+//     product_inStock: 0,
+//     product_price: 0,
+//     product_description: "",
+//     product_category: "nam",
+//     rating: 0,
+//   });
+
+//   const [thumbnailFile, setThumbnailFile] = useState(null);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setProduct((prev) => ({
+//       ...prev,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0];
+//     setThumbnailFile(file);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const formData = new FormData();
+//     for (let key in product) {
+//       formData.append(key, product[key]);
+//     }
+
+//     if (thumbnailFile) {
+//       formData.append("product_image", thumbnailFile);
+//     }
+//     setloading(true);
+//     try {
+//       const user = JSON.parse(localStorage.getItem("user"));
+//       const clientId = user._id;
+//       const response = await axiosInstance.post("/products/createProduct", formData, {
+//         headers: {
+//           "Content-Type": "multipart/form-data",
+//           "x-client-id": clientId
+//         },
+//         withCredentials: true,
+//       });
+//       setProduct({
+//         product_name: "",
+//         product_brand: "",
+//         product_inStock: 0,
+//         product_price: 0,
+//         product_description: "",
+//         product_category: "nam",
+//         rating: 0,
+//       })
+//       console.log("✅ Product created:", response.data);
+//       toast.success("Product created successfully!");
+//     } catch (error) {
+//       console.error("❌ Error creating product:", error);
+//       toast.error("Failed to create product.");
+//     }finally {
+//       setloading(false);
+//     }
+//   };
+
+//   return (
+//     <form
+//       onSubmit={handleSubmit}
+//       className="max-w-xl mx-auto p-6 bg-white shadow rounded-md space-y-4"
+//       encType="multipart/form-data"
+//     >
+//       <h2 className="text-xl font-semibold mb-4">Create Product</h2>
+
+//       <div>
+//         <label className="block mb-1">Product Name</label>
+//         <input
+//           type="text"
+//           name="product_name"
+//           value={product.product_name}
+//           onChange={handleChange}
+//           className="w-full border p-2 rounded"
+//           required
+//         />
+//       </div>
+
+//       <div>
+//         <label className="block mb-1">Product Brand</label>
+//         <input
+//           type="text"
+//           name="product_brand"
+//           value={product.product_brand.trim()}
+//           onChange={handleChange}
+//           className="w-full border p-2 rounded"
+//           required
+//         />
+//       </div>
+
+//       <div>
+//         <label className="block mb-1">In Stock</label>
+//         <input
+//           type="number"
+//           name="product_inStock"
+//           value={product.product_inStock}
+//           onChange={handleChange}
+//           className="w-full border p-2 rounded"
+//           min={0}
+//           required
+//         />
+//       </div>
+
+//       <div>
+//         <label className="block mb-1">Price</label>
+//         <input
+//           type="number"
+//           name="product_price"
+//           value={product.product_price}
+//           onChange={handleChange}
+//           className="w-full border p-2 rounded"
+//           min={0}
+//           required
+//         />
+//       </div>
+
+//       <div>
+//         <label className="block mb-1">Description</label>
+//         <textarea
+//         type="text"
+//           name="product_description"
+//           value={product.product_description}
+//           onChange={handleChange}
+//           className="w-full border p-2 rounded"
+//         ></textarea>
+//       </div>
+
+//       <div>
+//         <label className="block mb-1">Thumbnail</label>
+//         <input
+//           type="file"
+//           name="product_thumbnail"
+//           accept="image/*"
+//           onChange={handleFileChange}
+//           className="w-full border p-2 rounded"
+//           required
+//         />
+//       </div>
+
+//       <div>
+//         <label className="block mb-1">Category</label>
+//         <select
+//           name="product_category"
+//           value={product.product_category}
+//           onChange={handleChange}
+//           className="w-full border p-2 rounded"
+//         >
+//           <option value="nam">Nam</option>
+//           <option value="nữ">Nữ</option>
+//           <option value="đôi">Đôi</option>
+//         </select>
+//       </div>
+
+//       <button
+//         disabled={loading}
+//         type="submit"
+//         className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
+//       >
+//         {
+//           loading ? <span className="loading loading-dots loading-sm"></span>
+//           : "Create Product"
+//         }
+//       </button>
+//     </form>
+//   );
+// };
+
+
+
+
+
+
+{/* <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <Input label="Tên sản phẩm" name="product_name" />
+            <Input label="Thương hiệu" name="product_brand" />
+            <Input label="Giá" name="product_price" type="number" />
+            <Input label="Số lượng trong kho" name="product_inStock" type="number" />
+            <Select label="Loại sản phẩm" name="product_category" options={["nam", "nữ", "đôi"]} />
+            <Input label="Chất liệu vỏ" name="product_material" />
+            <Select label="Bộ máy" name="product_mechanism" options={["automatic", "quartz", "digital", "smartwatch"]} />
+            <Input label="Chống nước" name="product_water_resistance" />
+            <Input label="Đường kính mặt (mm)" name="product_case_diameter" type="number" />
+            <Select label="Kiểu dán mặt" name="product_dial_design" options={["Tròn/oval", "Vuông/Chữ nhật", "Minimalist (tối giản), Vintage, Sport"]} />
+            <Input label="Chất liệu dây" name="product_band_material" />
+            <Input label="Bảo hành" name="product_warranty" />
+          </div> */}
